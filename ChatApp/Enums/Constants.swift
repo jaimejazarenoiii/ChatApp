669c90476cfcd7778499f1677b193fad6f5a7af7
Jaime Jazareno III <jaime@churinc.com>
2020-04-26 04:46:03 +0800

    Add enums

---
+++ ChatApp/Enums/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by Jaime Jazareno III on 4/25/20.
+//  Copyright © 2020 Jaime Jazareno III. All rights reserved.
+//
+
+import UIKit
+
+/// Constants variable
+enum Constants {
+  static var screenWidth = UIScreen.main.bounds.width
+  static var screenHeight = UIScreen.main.bounds.height
+}
