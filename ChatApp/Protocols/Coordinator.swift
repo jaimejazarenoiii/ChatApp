8a24c606de544639dba9b6f2255e0b20b938c45f
Jaime Jazareno III <jaime@churinc.com>
2020-04-26 04:47:20 +0800

    Add coordinator and reusable protocol

---
+++ ChatApp/Protocols/Coordinator.swift
@@ -0,0 +1,15 @@
+//
+//  Coordinator.swift
+//  ChatApp
+//
+//  Created by Jaime Jazareno III on 4/24/20.
+//  Copyright © 2020 Jaime Jazareno III. All rights reserved.
+//
+
+import Foundation
+
+/// Protocol coordinator
+/// start() function should be called
+protocol Coordinator {
+    func start()
+}
